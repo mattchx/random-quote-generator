20070d2b480096f635fb0472ed2da9e6bc8a5ed2
Matthew Henderson <mattcharlesh@gmail.com>
2020-02-14 13:15:41 +0800

    1st dev commit, adding quote display

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       quotes: [],
       selectedQuoteIndex: null
     }
-    //if using this. -> we need to bind that function to the construtor 
+    //if a function uses this. -> we need to bind here to the construtor 
     this.selectQuoteIndex = this.selectQuoteIndex.bind(this);
   }
 
@@ -18,21 +18,31 @@ class App extends Component {
     fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
       .then(data => data.json())
       .then(quotes => this.setState({ quotes }, () => {
-        this.setState({selectQuoteIndex: this.selectQuoteIndex()})
+        this.setState({selectQuoteIndex: this.selectQuoteIndex() })
       })); //quotes:quotes object array 
   } 
 
+//get SelectedQuote () { // can call this function as if it were a variable
+ // if (!this.state.quotes.length || !Number.isInteger(this.state.quotes.length)){
+  //  return undefined;
+ // }
+  // returning the quote at the desired index
+ // return this.state.quotes[this.state.selectedQuoteIndex];
+//}
+
   selectQuoteIndex() {
       if (!this.state.quotes.length) {
-        return;
+        return; // undefined
       }
-      return Math.floor(Math.random() * this.state.quotes.length-1);
+      return Math.floor(Math.random() * this.state.quotes.length-1) + 1;
   }
 
   // render is run once state is updated
   render() { 
+    //console.log(this.state.selectQuoteIndex)
     return (
       <div className="App" id="quote-box">
+        {/*this.selectedQuote.quote? this.state.selectedQuote.quote : ''*/}
         <Button diplayButtonName="Click me" clickHandler={this.nextQuoteClickHandler} />
       </div>
     );
