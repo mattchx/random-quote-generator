44958791075e6370a19d3bf75c2fa2f49161f4d2
Matthew Henderson <mattcharlesh@gmail.com>
2020-02-15 15:04:02 +0800

    fully functional app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
 import 'typeface-roboto';
+import Grid from '@material-ui/core/Grid';
 import QuoteGenerator from './components/QuoteGenerator';
+import { withStyles } from '@material-ui/core/styles';
 
-class App extends Component {
+
+//import './App.css';
+
+const styles = {
+  container: {
+    alignItems: 'center',
+    display: 'flex',
+    height: '100vh',
+  }
+};
+
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -19,18 +31,19 @@ class App extends Component {
   componentDidMount() {
     fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
       .then(data => data.json())
-      .then(quotes => this.setState({ quotes },this.assignNewQuoteIndex)); //quotes:quotes object array 
+      .then(quotes => this.setState({ quotes }, this.assignNewQuoteIndex)); //quotes:quotes object array 
   }
 
   get selectedQuote() { // can call this function as if it were a variable
     if (!this.state.quotes.length || !Number.isInteger(this.state.selectedQuoteIndex)) {
+      // eslint-disable-next-line
       return; //undefined
     }
     return this.state.quotes[this.state.selectedQuoteIndex];
   } //returning the quote at the desired index
-/** 
-* Returns an integer representing an index in state.quotes
-**/
+  /** 
+  * Returns an integer representing an index in state.quotes
+  **/
   generateNewQuoteIndex() {
     if (!this.state.quotes.length) {
       return; // undefined
@@ -39,17 +52,25 @@ class App extends Component {
   }
 
   assignNewQuoteIndex() {
-    this.setState({selectedQuoteIndex : this.generateNewQuoteIndex() });
+    this.setState({ selectedQuoteIndex: this.generateNewQuoteIndex() });
   }
 
   // render is run once state is updated
   render() {
     return (
-      <div className="App" id="quote-box">
-        <QuoteGenerator selectedQuote={this.selectedQuote} assignNewQuoteIndex={this.assignNewQuoteIndex}/>
-      </div>
+      <Grid className={this.props.classes.container} id="quote-box" justify="center" container>
+        <Grid xs={11} lg={8} item>
+          {/*Only run QuoteGenerator when it recieves data*/}
+          {
+            this.selectedQuote ?
+              <QuoteGenerator selectedQuote={this.selectedQuote} assignNewQuoteIndex={this.assignNewQuoteIndex} />
+              : null
+          }
+        </Grid>
+      </Grid>
     );
   }
 }
 
-export default App;
+//export default App;
+export default withStyles(styles)(App);
--- src/components/QuoteGenerator.js
+++ src/components/QuoteGenerator.js
@@ -1,14 +1,40 @@
 import React from 'react';
-import Button from './Button';
-// import Typography from '@material-ui/core/Typography';
-    
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import IconButton from '@material-ui/core/IconButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faTwitter} from '@fortawesome/free-brands-svg-icons';
 
-const QuoteGenerator = (props) => (
-    //* <React.Fragment> = <> uses the quote and author keys from the props.state.quotes array*
-    <> 
-        {props.selectedQuote ? `"${props.selectedQuote.quote}" - ${props.selectedQuote.author}` : ''}
-        <Button buttonDisplayName="Next Quote" clickHandler={props.assignNewQuoteIndex} />
-    </>
+
+//* <React.Fragment> = <> uses the quote and author keys from the props.state.quotes array*
+// *Object destructuring* props -> {assignNewQuoteIndex, selectedQuote}
+// the argument props no longer has to be explicitly typed
+const QuoteGenerator = ({assignNewQuoteIndex, selectedQuote}) => (
+    <Card>
+        <CardContent>
+            {
+                selectedQuote ?
+                    (
+                        <Typography id="text">
+                            {selectedQuote.quote} - <span id="author">{selectedQuote.author}</span>
+                        </Typography>
+                    ) : null
+            }
+            <CardActions>
+                <Button id="new-quote" variant="contained" color='default' size='small' onClick={assignNewQuoteIndex}>Next Quote</Button>
+                <IconButton 
+                id="tweet-quote"
+                target="_blank"
+                href={encodeURI(`https://twitter.com/intent/tweet?text=${selectedQuote.quote} - ${selectedQuote.author}&hashtags=randomquote`)}
+                >
+                    <FontAwesomeIcon icon={faTwitter} size="md"></FontAwesomeIcon>
+                </IconButton>    
+           </CardActions>
+        </CardContent>
+    </Card>
 )
 
-export default QuoteGenerator
+export default QuoteGenerator;
